39ea1acf82ffb35063878119e18d8bb24cdd9274
Will Woodson <william.woodson@gsa.gov>
2017-11-03 12:51:05 -0500

    unique key names across environments

--- ci/new-ca.sh
+++ ci/new-ca.sh
@@ -18,7 +18,7 @@ cp secrets-in/secrets.yml secrets-updated/secrets.yml
 
 # Append CA certificate to secrets
 spruce json secrets-updated/secrets.yml \
-  | jq --arg cert "$(cat out/master-bosh.crt)" '.secrets.ca_cert = (.secrets.ca_cert + "\n" + $cert)' \
+  | jq --arg cert "$(cat out/master-bosh.crt)" '.ca_cert = (.ca_cert + "\n" + $cert)' \
   | spruce merge \
   > secrets-updated/tmp.yml
 mv secrets-updated/tmp.yml secrets-updated/secrets.yml
@@ -27,14 +27,14 @@ mv secrets-updated/tmp.yml secrets-updated/secrets.yml
 ## this public key is stored in ec2 and must be rotated on all bosh deployments
 ##
 spruce json secrets-updated/secrets.yml \
-  | jq --arg key "masterbosh-$(date +'%Y%m%d')" '.secrets.ca_public_key_name = $key' \
+  | jq --arg key "masterbosh-$(date +'%Y%m%d')" '.ca_public_key_name = $key' \
   | spruce merge \
   > secrets-updated/tmp.yml
 mv secrets-updated/tmp.yml secrets-updated/secrets.yml
 
 # Append CA private key to secrets
 spruce json secrets-updated/secrets.yml \
-  | jq --arg key "$(cat out/master-bosh.key)" '.secrets.ca_key = (.secrets.ca_key + "\n" + $key)' \
+  | jq --arg key "$(cat out/master-bosh.key)" '.ca_key = (.ca_key + "\n" + $key)' \
   | spruce merge \
   > secrets-updated/tmp.yml
 mv secrets-updated/tmp.yml secrets-updated/secrets.yml
